52144c9e8f14e01cc192bfb62e18a26de9c855b6
paulovitor21 <paulovitorpereira21@outlook.com>
2025-04-28 14:22:24 -0400

    agora usando a lib yt_dlp

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import logging
 import os
 from flask import Flask, render_template, request
-from pytube import YouTube
-from tqdm import tqdm
+import yt_dlp
 
 app = Flask(__name__, template_folder='templates')
 
@@ -10,25 +9,39 @@ app = Flask(__name__, template_folder='templates')
 logging.basicConfig(level=logging.INFO)
 
 # Caminho do diretório de download
-DOWNLOAD_DIR = os.path.join(os.path.expanduser('~'), 'Downloads')
+DOWNLOAD_DIR = os.path.join(os.path.expanduser('~'), 'Downloads\musicas')
 
-class TqdmUpTo(tqdm):
-    def update_to(self, stream, chunk, bytes_remaining):
-        if self.total is None:
-            self.total = bytes_remaining
-        self.update(len(chunk))
+# Caminho do ffmpeg.exe
+FFMPEG_PATH = r"C:\ffmpeg\bin\ffmpeg.exe"  # <<< MUITO IMPORTANTE: coloque o caminho correto aqui
 
-def download_video(url, output_path):
+def download_audio(url, output_path):
     try:
-        yt = YouTube(url)
-        stream = yt.streams.get_highest_resolution()
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'ffmpeg_location': FFMPEG_PATH,  # <<< Aqui está passando o caminho do ffmpeg
+            'progress_hooks': [my_hook],
+            'noplaylist': True,
+            'quiet': False,
+        }
 
-        with TqdmUpTo(unit='bytes', unit_scale=True, desc=stream.title) as progress_bar:
-            yt.register_on_progress_callback(progress_bar.update_to)
-            stream.download(output_path)
-        app.logger.info("Download concluído")
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+
+        app.logger.info("Download de áudio concluído")
     except Exception as e:
-        app.logger.error(f"Erro: {e}")
+        app.logger.error(f"Erro durante o download: {e}")
+
+def my_hook(d):
+    if d['status'] == 'downloading':
+        pass
+    if d['status'] == 'finished':
+        app.logger.info("Download finalizado, convertendo...")
 
 @app.route('/')
 def index():
@@ -37,7 +50,7 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     video_url = request.form['video_url']
-    download_video(video_url, DOWNLOAD_DIR)
+    download_audio(video_url, DOWNLOAD_DIR)
     return "O download está em andamento. Verifique os logs para obter o progresso."
 
 if __name__ == "__main__":
